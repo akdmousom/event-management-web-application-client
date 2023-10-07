6c7b919929c7abd62c1c32b1e21562142572cf48
AKD <contact.mousom@gmail.com>
2023-10-07 13:50:15 +0600

    Footer is now added to this website

--- src/MainLayout/MainLayout.jsx
+++ src/MainLayout/MainLayout.jsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../Components/Footer/Footer";
+import NavBar from "../Components/Header/NavBar/NavBar";
 
 const MainLayout = () => {
     return (
         <div className=" font-poppins">
+            <NavBar></NavBar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,9 @@
 import Banner from "../../Components/Header/Banner/Banner";
-import NavBar from "../../Components/Header/NavBar/NavBar";
 import Support from "../../Components/MainSection/Support/Support";
 
 const Home = () => {
     return (
         <div>
-            <NavBar />
             <Banner />
             <Support />
         </div>
